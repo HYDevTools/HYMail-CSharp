493760eb85d8828d0228ce3680106b6bb02d2a1f
hyc <519544044@qq.com>
2018-01-07 16:03:33 +0800

    调整

--- HY.MailTests/QQMailSenderTests.cs
+++ HY.MailTests/QQMailSenderTests.cs
@@ -9,9 +9,9 @@ namespace Hy.Mail.Tests
         [TestMethod()]
         public void QQMailSenderTest()
         {
-            //var test = new QQMailSender("519544044@qq.com", "wjpygxjumutsbgdh");
-            //test.Send("519544044@qq.com", "测试邮件", "邮件正文");
-            var test2 = new QQMailSender("519544044@qq.com", "wjpygxjumutsbgdh", "519544044@qq.com", "519544044@qq.com");
+            //var test = new QQMailSender("XX@qq.com", "");
+            //test.Send("519544044@qq.com", "测试邮件", "邮件正文");X
+            var test2 = new QQMailSender("XX@qq.com", "XXXX", "XXX@qq.com", "XX@qq.com");
             test2.Send("预设测试邮件", "邮件正文");
             Assert.IsTrue(true);
         }
