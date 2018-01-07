5153d8ad714aa62cb5e37fc5b729ba53c280157e
hyc <519544044@qq.com>
2018-01-07 13:32:47 +0800

    类型 注释调整

--- HY.Mail/Base/MailSender.cs
+++ HY.Mail/Base/MailSender.cs
@@ -5,7 +5,7 @@ using System.Net.Mail;
 
 namespace HY.Mail.Base
 {
-    public abstract class MailSender
+    public class MailSender
     {
         private MailAddress _fromMailAdress;
         private readonly SmtpClient _mailclient;
@@ -21,14 +21,14 @@ namespace HY.Mail.Base
         /// <param name="mailAddress">邮件账户</param>
         /// <param name="password">邮件密码</param>
 
-        public MailSender(string host, int port, string mailAddress, string password, string to = "", string cc = "", string title = "",bool enableSsl=true)
+        public MailSender(string host, int port, string mailAddress, string password, string to = "", string cc = "", string title = "", bool enableSsl = true)
         {
             MailAddress = mailAddress;
-            _mailclient = GetSmtpClient(host, port, mailAddress, password,enableSsl);
+            _mailclient = GetSmtpClient(host, port, mailAddress, password, enableSsl);
             _to = to;
             _cc = cc;
         }
-        public SmtpClient GetSmtpClient(string host, int port, string from, string password, bool enableSsl = true)
+        private SmtpClient GetSmtpClient(string host, int port, string from, string password, bool enableSsl = true)
         {
             var key = $"{host}_{port}_{from}_{password}";
             SmtpClient client = null;
@@ -58,6 +58,11 @@ namespace HY.Mail.Base
         {
             Send(to, "", title, content);
         }
+        /// <summary>
+        /// 发送邮件(构造函数需要预设收件人)
+        /// </summary>
+        /// <param name="title">标题</param>
+        /// <param name="content">正文</param>
         public virtual void Send(string title, string content)
         {
             if (string.IsNullOrEmpty(_to))
@@ -71,7 +76,7 @@ namespace HY.Mail.Base
         /// <param name="cc">抄送人,多个逗号隔开</param>
         /// <param name="title">标题</param>
         /// <param name="content">正文</param>
-        public virtual void Send(string to, string cc, string title, string content)
+        public void Send(string to, string cc, string title, string content)
         {
             if (string.IsNullOrEmpty(_to))
                 throw new ArgumentException("Mail Recevier Address Cannt Be Null", "to");
